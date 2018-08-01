9590e74823cdf2fe1cdc023f30f43f61d18172e0
reliance <reliance@atguigu.com>
2018-08-01 14:14:24 +0800

    gmall最新版

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/mapper/BaseCatalog3Mapper.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.gmall.manage.mapper;
-
-import com.atguigu.gmall.bean.BaseCatalog2;
-import com.atguigu.gmall.bean.BaseCatalog3;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BaseCatalog3Mapper extends Mapper<BaseCatalog3> {
-}
